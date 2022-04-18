bf401afc449573eaa81ab9d258201aa3bd460baf
Jomy george <87518944+jomyg@users.noreply.github.com>
2022-04-18 18:05:31 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,18 @@
+import boto3
+
+def lambda_handler(event, context):
+   
+  ec2 = boto3.client('ec2',region_name="ap-southeast-1")
+  
+  instances = ec2.describe_instances(Filters=[  {'Name':'tag:Name', 'Values':["webserver"]},
+                                              {'Name':'tag:Project', 'Values':["zomato"]}
+                                              
+                                           ]
+                                    )
+
+  for item in instances['Reservations']:
+    
+    instance = item['Instances'][0]
+    instance_id = instance['InstanceId']
+    print("Stopping Instance : {}".format(instance_id))
+    ec2.start_instances(InstanceIds=[ instance_id ])
